506360ffb23c05e2e8c3e856edd88d0265660f06
Brian Kyole <tribeless53@gmail.com>
2020-07-18 21:27:14 +0300

    projects & why me

--- public/Js/script.js
+++ public/Js/script.js
@@ -1,3 +1,4 @@
+//name()
 fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@briankyole10")
 .then(response=>response.json())
 .then(
@@ -13,3 +14,19 @@ fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@bri
 )
 .catch(error => console.log(error))
     
+// async function name(){
+//     await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts', {
+//         owner: 'tribeless',
+//         repo: 'My-Portfolio'
+//     })
+// }
+
+fetch('https://api.github.com/users/tribeless/repos')
+.then(response=>response.json())
+.then(data=>{
+// console.log(data)
+data.forEach(item=>{
+    console.log(item.name, item.url, item.description, item.owner.login, item.language);
+})
+})
+.catch(error=>console.log(error))
