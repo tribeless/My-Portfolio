b625953c2a4df59c8e341dfeb1d69b249e285288
Brian Kyole <tribeless53@gmail.com>
2020-07-19 22:41:24 +0300

    new commit & bug fixes

--- public/Js/script.js
+++ public/Js/script.js
@@ -71,9 +71,9 @@ fetch('https://api.github.com/users/tribeless/repos')
 
 const myPrefferedLanguages = ["HTML", "CSS", "JavaScript"];
 const preferredIndex = [3,4,5,6];
-let output = "";
+
 const filteredItems = data.filter(item=>{
-     
+    
     for(var i=0;i<myPrefferedLanguages.length;i++){
         if (item.language === myPrefferedLanguages[i])
         return item;
@@ -81,21 +81,22 @@ const filteredItems = data.filter(item=>{
 })
     for(var i=0;i<filteredItems.length;i++){
         for(var j=0;j<preferredIndex.length;j++){
+            var output = "";
             var existingItems = filteredItems.indexOf(filteredItems[i]);
             var numbersIndex = preferredIndex[j];
             if(existingItems===numbersIndex){
                 var newElement = document.createElement("div");
-                var parentElement = document.querySelector(".open");
+                var parentElement = document.querySelector(".my-projects");
                 output+=
                 `
-                        <a href="#" class="box">
+                        <a href="#" target="blank" class="box">
                         <div class="container">
                         <div class="contents">
                             <div class="name"><i class="fas fa-project-diagram tt" ></i>${filteredItems[numbersIndex].name}</div>
                             <div class="description">${filteredItems[numbersIndex].description}</div>
                             <span class="language"><i class="fas fa-code tt"></i>${filteredItems[numbersIndex].language}</span>
-                            <span class="github-link"><a href="123"
-                                    style="text-decoration:none;color:white;margin-right:1rem;"><i class="fab fa-github tt"></i>${filteredItems[i].html_url}</a></span>
+                            <span class="github-link">
+                            <a style="text-decoration:none;color:white;margin-right:1rem;" href="${filteredItems[numbersIndex].html_url}" target="blank"><i class="fab fa-github tt"></i>View Code</a></span>
                             <span class="user-name"><i class="fas fa-user tt"></i>${filteredItems[numbersIndex].owner.login}</span>
                         </div>
                     </div>
@@ -103,7 +104,7 @@ const filteredItems = data.filter(item=>{
                 `;
                 newElement.innerHTML = output;
                 parentElement.appendChild(newElement);
-                
+                console.log(filteredItems[numbersIndex])
             }
             else{
                 continue;
